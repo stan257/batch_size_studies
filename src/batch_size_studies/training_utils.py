[FIX][ADAM] Major fix for appropriate scaling of learning rate of ADAM for width

src/batch_size_studies/training_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import optax
 
 from .definitions import OptimizerType, Parameterization
@@ -9,21 +10,33 @@ def eta_adjustment_fn(experiment, eta: float):
     "The Optimization Landscape of SGD Across the Feature Learning Strength"
     Atanasov et al. (2025, arXiv:2410.04642)
 
-    For muP, we scale by the width N to ensure μ-transfer across width.
+    For μP, we scale by the width N for SGD (resp. sqrt(N) for ADAM) to ensure
+    μ-transfer across width.
     """
     gamma = experiment.gamma
     depth = experiment.L
+    width = experiment.N
+
     match experiment.optimizer:
         case OptimizerType.SGD:
-            mult = gamma ** (2 / depth) if gamma > 1 else gamma**2
+            gamma_mult = gamma ** (2 / depth) if gamma > 1 else gamma**2
         case OptimizerType.ADAM:
-            mult = gamma ** (1 / depth) if gamma > 1 else gamma
+            gamma_mult = gamma ** (1 / depth) if gamma > 1 else gamma
         case _:
             # Default to returning the base eta if no specific rule is defined.
-            mult = 1.0
-    base_lr = eta * mult
-    if experiment.parameterization == Parameterization.MUP:
-        return base_lr * experiment.N
+            gamma_mult = 1.0
+
+    if experiment.parameterization == Parameterization.SP:
+        width_mult = 1.0
+    else:
+        match experiment.optimizer:
+            case OptimizerType.SGD:
+                width_mult = width
+            case OptimizerType.ADAM:
+                width_mult = np.sqrt(width)
+            case _:
+                width_mult = 1.0
+    base_lr = eta * gamma_mult * width_mult
     return base_lr
 
 